5ec15c0f4f15dd30b661d670b44c7075133f8840
Nightseeing <thenightseeing@gmail.com>
2025-12-28 23:20:06 +0100

    Adding new week days and better sleep
    Adding colour-based week days, improved formatting of the date and sleep subtracting compute time from delay

--- timekeeping.py
+++ timekeeping.py
@@ -5,25 +5,27 @@ import math
 
 epoch = datetime(1953,5,24)
 
-urodziny = datetime(1990,5,13,5,40)
-
-#mmonthname = ["Alfa","Beta","Gamma","Delta","Epsilon","Zeta",
-#              "Eta","Theta","Iota","Kappa","Lambda","Mu",
-#              "Nu","Ksi","Omicron","Pi","Rho","Sigma",
-#              "Tau","Upsilon","Phi","Chi","Psi","Omega"]
-mmonthname = ["Fehu","Uruz","Thurisaz","Ansuz","Raido","Kaunan",
-		"Gebo","Wunjo","Hagalaz","Naudiz","Isaz","Jera",
-		"Eihwaz","Perth","Algiz","Sowilo","Tiwaz","Berkanan",
-		"Ehwaz","Mannaz","Laguz","Ingwaz","Othala","Dagaz"]
-mweekdayname = ["Sunday","Monday","Tuesday","Wednesday",
-                "Thursday","Friday","Saturday"]
+mmonthname = ["Alfa","Beta","Gamma","Delta","Epsilon","Zeta",
+              "Eta","Theta","Iota","Kappa","Lambda","Mu",
+              "Nu","Ksi","Omicron","Pi","Rho","Sigma",
+              "Tau","Upsilon","Phi","Chi","Psi","Omega"]
+#mmonthname = ["Fehu","Uruz","Thurisaz","Ansuz","Raido","Kaunan",
+#		"Gebo","Wunjo","Hagalaz","Naudiz","Isaz","Jera",
+#		"Eihwaz","Perth","Algiz","Sowilo","Tiwaz","Berkanan",
+#		"Ehwaz","Mannaz","Laguz","Ingwaz","Othala","Dagaz"]
+#mweekdayname = ["Sunday","Monday","Tuesday","Wednesday",
+#                "Thursday","Friday","Saturday"]
+mweekdayname = ["\033[45mViolet\033[0m","\033[101mRed\033[0m",
+                "\033[43mOrange\033[0m","\033[103mYellow\033[0m",
+                "\033[102mGreen\033[0m","\033[106mBlue\033[0m",
+                "\033[104mIndigo\033[0m"]
 sekwmrok = 59390475
 sekwmsol = 88775
 
 while True:
 
-    teraz = datetime.today()
-    roznica = teraz - epoch
+    teraz1 = datetime.today()
+    roznica = teraz1 - epoch
     roznisek = roznica.days * 86400 + roznica.seconds
     roznimlat = roznisek // sekwmrok
     roznimsol = (roznisek % sekwmrok) // sekwmsol
@@ -40,11 +42,13 @@ while True:
     mgodz = rozniresztsek // 3600
     mmin = (rozniresztsek % 3600) // 60
     msek = (rozniresztsek % 3600) % 60
-    print('czas lokalny:',teraz)
+    print('czas lokalny:',teraz1)
     #print(roznica)
-
-    print('data marsjańska:',msol,mmonthname[mmsc],mrok,mweekdayname[mtygd],mtyg)
-    print('czas marsjański:',mgodz,':',mmin,':',msek)
+    print('data marsjańska:',mweekdayname[mtygd],msol,mmonthname[mmsc],mrok)
+    print(f'data marsjańska: {msol}.{mmsc:02d}.{mrok} | {mtygd}-{mtyg}')
+    print(f'czas marsjański: {mgodz:02d}:{mmin:02d}:{msek:02d}')
     print('========================================')
-    sleep(600)
-
+    teraz2 = datetime.now()
+    dt = teraz2 - teraz1
+    print(dt.total_seconds()*1000000,"us")
+    sleep(60-dt.total_seconds()*2)
